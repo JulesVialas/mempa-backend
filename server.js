26f637646a74e49b0c1eb1694a4266e148326a6b
Jules Vialas <jules.vialas@gmail.com>
2026-02-05 22:34:46 +0100

    Ajoutez des commentaires pour documenter la configuration et le démarrage du serveur HTTP.

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+/**
+ * server.js                05/02/2026
+ * MIAGE de Toulouse, pas de copyright
+ */
+
 const http = require('http');
 const app = require('./app');
+
+/** Chargement des variables d'environnement */
 require('dotenv').config();
 
+/** Configuration du port */
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
+/** Création du serveur HTTP */
 const server = http.createServer(app);
 
+/** Démarrage du serveur */
 server.listen(port, () => {
     console.log(`Serveur Backend démarré sur le port ${port}`);
 });
